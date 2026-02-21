c9e54a015cbe914a745a922df7cb47408e41704a
christianlinde <info@christian-linde.de>
2026-02-21 09:01:58 +0100

    fix immutable collection error

--- src/main/java/org/akadia/prometheus/velocity/metrics/OnlinePlayerConnectionAddress.java
+++ src/main/java/org/akadia/prometheus/velocity/metrics/OnlinePlayerConnectionAddress.java
@@ -6,10 +6,7 @@ import org.akadia.prometheus.interfaces.GauageMetric;
 import org.akadia.prometheus.velocity.PrometheusVelocityExporter;
 
 import java.net.InetSocketAddress;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 public class OnlinePlayerConnectionAddress extends GauageMetric {
 
@@ -20,7 +17,7 @@ public class OnlinePlayerConnectionAddress extends GauageMetric {
     public void doCollect() {
         this.getGauge().clear();
 
-        Map<String, List<String>> playerAddresses = new HashMap<String, List<String>>();
+        Map<String, Set<String>> playerAddresses = new HashMap<String, Set<String>>();
 
         ProxyServer proxy = ((PrometheusVelocityExporter) getPlugin()).getProxyServer();
         proxy.getAllPlayers().forEach(player -> {
@@ -28,20 +25,19 @@ public class OnlinePlayerConnectionAddress extends GauageMetric {
 
             String address = virtualHost.isPresent() ? virtualHost.get().getHostName() : "unknown";
             if(!playerAddresses.containsKey(address)) {
-                playerAddresses.put(address, List.of(player.getUsername()));
-            } else {
-                List<String> players = playerAddresses.get(address);
-                players.add(player.getUsername());
-                playerAddresses.put(address, players);
+                playerAddresses.put(address, new HashSet<>());
             }
+            Set<String> players = playerAddresses.get(address);
+            players.add(player.getUsername());
+            playerAddresses.put(address, players);
         });
 
-        for(Map.Entry<String, List<String>> addressEntry : playerAddresses.entrySet()) {
+        for(Map.Entry<String, Set<String>> addressEntry : playerAddresses.entrySet()) {
             String address = addressEntry.getKey();
-            this.getGauge().labels(address, "").set(addressEntry.getValue().size());
             for(String player : addressEntry.getValue()) {
                 this.getGauge().labels(address, player).set(1);
             }
+            this.getGauge().labels(address, "").set(addressEntry.getValue().size());
         }
     }
 
